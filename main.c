5a21c2514b1e11612fe2665d2b7b33902ff37310
Mykolas Dzidzevičius <108366398+dz-mykolas@users.noreply.github.com>
2023-03-01 12:36:07 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <argp.h>
+#include <string.h>
 
 #include "ram_usage.h"
-
 #include "cJSON.h"
 #include "tuya_cacert.h"
 #include "tuya_log.h"
@@ -19,6 +19,8 @@ struct my_args {
 
 tuya_mqtt_context_t client_instance;
 
+bool isOnline = true;
+
 void on_connected(tuya_mqtt_context_t *context, void *user_data)
 {
     TY_LOGI("on connected");
@@ -31,18 +33,29 @@ void on_disconnect(tuya_mqtt_context_t *context, void *user_data)
 
 void message_helper(char* s)
 {
-    printf("%s", s);
+    if (strcmp(s, "func1") == 0) {
+        printf("%s\n", s);
+        return;
+    }
+    if (strcmp(s, "disconnect") == 0) {
+        isOnline = false;
+        printf("Disconnected\n");
+        return;
+    }
 }
 
 void on_messages(tuya_mqtt_context_t *context, void *user_data, const tuyalink_message_t *msg)
 {
     TY_LOGI("on message id:%s, type:%d, code:%d", msg->msgid, msg->type, msg->code);
+    cJSON *json = cJSON_Parse(msg->data_string);
+    char *identifier = json->child->string;
     switch (msg->type) {
         case THING_TYPE_PROPERTY_SET:
             TY_LOGI("property set:%s", msg->data_string);
-            // msg struct has cJSON. Can rework
-            cJSON *json = cJSON_Parse(msg->data_string);
-            char *identifier = json->child->string;
+            message_helper(identifier);
+            break;
+        case THING_TYPE_ACTION_EXECUTE:
+            identifier = json->child->next->valuestring;
             message_helper(identifier);
             break;
         default:
@@ -73,16 +86,14 @@ int main(int argc, char **argv)
                                      .on_messages = on_messages});
 
     assert(ret == OPRT_OK);
-
     ret = tuya_mqtt_connect(client);
     assert(ret == OPRT_OK);
-
-    for (;;)
+    while (isOnline)
     {
-        /* Loop to receive packets, and handles client keepalive */
         ram_print_free();
         tuya_mqtt_loop(client);
     }
-
+    tuyalink_thing_property_report(client, deviceId, "Some data");
+    ret = tuya_mqtt_disconnect(client);
     return ret;
 }
